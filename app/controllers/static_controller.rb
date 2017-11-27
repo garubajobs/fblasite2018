5e0475234767d42681a073be758bf3ddb2a48cc2
Ayush Nene <ayush3108@gmail.com>
2017-11-26 21:19:28 -0500

    test

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,5 @@
 class StaticController < ApplicationController
-  before_action :setup_vars
-  def setup_vars
-    @links = %w(openings resume application interview)
-    @link_text = %w(openings r&eacute;sum&eacute; application interview)
-  end
+
   def index
   end
 
