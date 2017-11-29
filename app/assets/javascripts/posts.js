db2d6b3b5ecc0da83b81ce4db202054ad3523b32
Ayush Nene <ayush3108@gmail.com>
2017-11-29 07:24:44 -0500

    fix stuff

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -12,13 +12,27 @@ var showModalPost = function() {
     $('.obfuscator').show().on('click', hideModalPost)
     $(this).parents('.card').next('.modal').show()
 }
+
 //hide the modal with all replies
 var hideModalPost = function() {
     $(this).off().hide()
     $('.modal').hide()
 }
 
-$(function() {
+//check email in form
+function ValidateEmail(text)   
+{  
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(text))  
+  {  
+    return (true)  
+  }
+    return (false)  
+}  
+
+$(document).on('turbolinks:load', function() {
     $(document).on ('click', '.reply-placeholder', displayReplyForm)
     $(document).on ('click', '.card .post-content, .card .reply', showModalPost)
+    $(document).on ('change', 'input[name*="name"]', function() {
+        
+    })
 })
