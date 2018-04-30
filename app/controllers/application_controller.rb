0d5cd2846311b5e738f3ff90cac1f82312a9062f
Ayush Nene <ayush3108@gmail.com>
2018-04-30 10:18:47 -0400

    Added Forum to tabs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :setup_vars
   def setup_vars
-    @links = %w(openings resume application interview)
-    @link_text = %w(openings r&eacute;sum&eacute; application interview)
+    @links = %w(openings resume application interview forum)
+    @link_text = %w(openings r&eacute;sum&eacute; application interview forum)
   end
 end
